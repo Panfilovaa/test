8948419a78b01e5f1a4ed8272d34d6047d26c91b
Anastasia <anppna@mail.ru>
2020-11-19 21:28:39 +0400

    Выгрузка тестовых заданий 1 и 2

---
+++ src/main/java/ru/neoflex/Panfilova/User.java
@@ -0,0 +1,28 @@
+package ru.neoflex.Panfilova;
+
+import java.util.Date;
+
+public class User {
+
+    private String username;
+    private String city;
+    private Date date;
+
+    User(String username, String city, Date date){
+        this.username = username;
+        this.city = city;
+        this.date = date;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
